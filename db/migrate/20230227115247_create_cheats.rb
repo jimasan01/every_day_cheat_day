abda91bee6fef657670932dd28ca627efcdb2326
jimasan01 <takayuki01200216@gmail.com>
2023-03-12 16:10:07 +0900

    cheatsテーブル作成

---
+++ db/migrate/20230227115247_create_cheats.rb
@@ -0,0 +1,16 @@
+class CreateCheats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cheats do |t|
+      t.string      :name,         null: false
+      t.text        :explanation,  null: false
+      t.integer     :volume_id,    null: false
+      t.integer     :price_id,     null: false
+      t.integer     :category_id,  null: false
+      t.integer     :area_id,      null: false
+      t.string      :city,         null: false
+      t.string      :house_number, null: false
+      t.string      :building
+      t.timestamps
+    end
+  end
+end
